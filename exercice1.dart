815d3fcb8a99cdbfcf225c6d413212115ab40a6a
amirasadouni00 <amirasadouni5@gmail.com>
2025-11-13 18:13:20 -0500

    Exercice 1 Q5: Ajout de la méthode afficherCouleur() pour afficher les couleurs

---
+++ exercice1.dart
@@ -0,0 +1,28 @@
+class Couleur {
+  final int r;
+  final int g;
+  final int b;
+
+  Couleur(this.r, this.g, this.b);
+
+  Couleur.rouge() : r = 255, g = 0, b = 0;
+  Couleur.vert() : r = 0, g = 255, b = 0;
+  Couleur.bleu() : r = 0, g = 0, b = 255;
+
+  void afficherCouleur() {
+    print("(R:$r, V:$g, B:$b)");
+  }
+}
+
+void main() {
+  var rouge = Couleur.rouge();
+  var vert = Couleur.vert();
+  var bleu = Couleur.bleu();
+
+  rouge.afficherCouleur();
+  vert.afficherCouleur();
+  bleu.afficherCouleur();
+
+  var maCouleur = Couleur(128, 64, 255);
+  maCouleur.afficherCouleur();
+}
