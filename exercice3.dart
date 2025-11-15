b9d34cae126d284dd8b001297ea9178457b9bae8
amirasadouni00 <amirasadouni5@gmail.com>
2025-11-14 21:01:03 -0500

    Ex3 Q1-Q5 : Classe Compte avec _solde, numeroCompte, constructeur et getter solde

--- exercice3.dart
+++ exercice3.dart
@@ -5,9 +5,14 @@ class Compte {
   Compte(this.numeroCompte, [double solde = 0.0]) {
     _solde = solde;
   }
+
+  String get solde {
+    return "${_solde.toStringAsFixed(2)} \$";
+  }
 }
 
 void main() {
-  var c = Compte("A100", 20.0);
-  print("Compte créé : ${c.numeroCompte}");
+  var compte = Compte("C001", 100.0);
+  print("Compte créé : ${compte.numeroCompte}");
+  print("Solde actuel : ${compte.solde}");
 }
