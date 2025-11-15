3d2fdab0eaf2bd68abb672f8dd43ca2eecb38312
amirasadouni00 <amirasadouni5@gmail.com>
2025-11-14 21:31:12 -0500

    Q1-Q2 : Création de l'interface Connectable et de la classe ServeurAPI

---
+++ exercice6.dart
@@ -0,0 +1,23 @@
+abstract class Connectable {
+  void connecter(String utilisateur);
+  void deconnecter();
+}
+
+class ServeurAPI implements Connectable {
+  @override
+  void connecter(String utilisateur) {
+    print("ServeurAPI : Connexion établie pour $utilisateur.");
+  }
+
+  @override
+  void deconnecter() {
+    print("ServeurAPI : Déconnexion réussie.");
+  }
+}
+
+void main() {
+  ServeurAPI serveur = ServeurAPI();
+
+  serveur.connecter("Amira");
+  serveur.deconnecter();
+}
