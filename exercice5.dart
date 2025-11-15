a9b7b7b6071359fe312437aa3c385880566768fa
amirasadouni00 <amirasadouni5@gmail.com>
2025-11-14 21:18:21 -0500

    Ex5 Q1-Q5 : Classe Cercle avec propriété rayon et méthode calculerAire()

--- exercice5.dart
+++ exercice5.dart
@@ -6,6 +6,19 @@ abstract class Forme {
   }
 }
 
+class Cercle extends Forme {
+  final double rayon;
+
+  Cercle(this.rayon);
+
+  @override
+  double calculerAire() {
+    return 3.14159 * rayon * rayon;
+  }
+}
+
 void main() {
-  print("Forme abstraite prête.");
+  var cercle = Cercle(5.0);
+  cercle.afficherMessage();
+  print("Aire du cercle : ${cercle.calculerAire()}");
 }
