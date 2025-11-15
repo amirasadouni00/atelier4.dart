be89ecb042691cc140b34b1db340d7254740602f
amirasadouni00 <amirasadouni5@gmail.com>
2025-11-14 20:47:19 -0500

    Ex2 Q1-Q3: Classe Point avec attributs privés, constructeur et méthode move

--- exercice2.dart
+++ exercice2.dart
@@ -1,10 +1,16 @@
-// Exercice 2 – Point dans le plan
-
 class Point {
   double _x;
   double _y;
 
   Point(this._x, this._y);
+
+  void move(double dx, double dy) {
+    _x += dx;
+    _y += dy;
+  }
 }
 
-void main() {}
+void main() {
+  var pt = Point(2, 5);
+  pt.move(-1, 3);
+}
