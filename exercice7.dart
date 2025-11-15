26714e0bc47992758d54d6a12d47069bbc3a0e59
amirasadouni00 <amirasadouni5@gmail.com>
2025-11-14 21:46:18 -0500

    Q1-Q4 : Ajout de l'incrémentation du compteur dans le constructeur

--- exercice7.dart
+++ exercice7.dart
@@ -1,13 +1,19 @@
 class Tache {
   String description;
-
   static int nombreTotal = 0;
 
-  Tache(this.description);
+  Tache(this.description) {
+    nombreTotal++;
+  }
 }
 
 void main() {
   Tache t1 = Tache("Faire les courses");
-  print("Tâche créée : ${t1.description}");
-  print("Nombre total de tâches (avant incrément) : ${Tache.nombreTotal}");
+  Tache t2 = Tache("Répondre aux emails");
+  Tache t3 = Tache("Préparer le rapport");
+
+  print(
+    "Tâches créées : ${t1.description}, ${t2.description}, ${t3.description}",
+  );
+  print("Nombre total de tâches : ${Tache.nombreTotal}");
 }
